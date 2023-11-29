f26193b18ab7c7e168679a478b42db7af82b83a0
Jupy <jupiterian.is@gmail.com>
2023-11-29 20:23:48 +0000

    killing children and beginning dicts

--- graph_traversal.py
+++ graph_traversal.py
@@ -1,42 +1,28 @@
 class Node:
-    #node class, stores the state of the puzzle, as well as children and parent nodes
+    #node class, stores the state of the puzzle
     def __init__(self, state):
-        self.parent = None 
-        self.children = [] #states that can be reached from the current state
         self.state = state #contains the entire puzzle state
-        self.g = 0 #depth of the node
-        self.f = self.get_f_value() #heuristic value of the node
     
-    def create_child(self, state): #represent a new state reachable from the *current* state
-        child = Node(state) #create a new node
-        child.parent = self #parent of node is current node
-        self.children.append(child) #add child to children list
-        child.g = self.g + 1 #depth of child is depth of parent + 1
-    
-    def get_f_value(self): #calculate the heuristic value of the node
-        h = len(self.peg3) #number of disks on peg 3
-        #heuristic value is the number of disks on peg 3
-        return h + self.g #f = g + h
-
-    def goal_test(self, goal): #have we solved the puzzle?
+    def goal_test(self, goal=None): #have we solved the puzzle?
         return None
 
     def print_puzzle(self): #display the state of the puzzle at each step
         print(self.state) #print 'peg' array
 
     def generate_moves(self):
-     if self.children == []: #only generate moves once ie. if the children list is empty
-      for peg_ind, inital_peg in enumerate(self.puzzle):  # for each peg
-        if len(inital_peg) != 0: # if the initial peg is not empty
-            disk = inital_peg[-1] # -1 index gives the top disk
-            for index, target_peg in enumerate(self.puzzle): # for each peg
-                if peg != index: # as long as the pegs are not the same
-                    new_puzzle = [peg_ind.copy() for peg in self.puzzle] # make a copy of the puzzle
-                    new_puzzle[peg_ind].remove(disk)
-                    new_puzzle[index].append(disk)
-                    # in the new puzzle perform the move
-                    if len(target_peg) == 0 or target_peg[-1] > disk:
-                        self.create_child(*new_puzzle) # form a new node from the new state if the move is safe
-                        print(f"Safe move from {self.puzzle} to {new_puzzle} generated.") #generate safe state
-                    else:
-                        print(f"Unsafe move {self.puzzle} to {new_puzzle} marked as dead-end.") #mark unsafe states and do not explore them further
+        children = []
+        for peg_ind, inital_peg in enumerate(self.puzzle):  #for each peg
+            if len(inital_peg) != 0: #if the initial peg is not empty
+                disk = inital_peg[-1] #-1 index gives the top disk
+                for index, target_peg in enumerate(self.puzzle): #for each peg
+                    if peg_ind != index: #as long as the pegs are not the same
+                        new_puzzle = [peg_ind.copy() for peg in self.puzzle] #make a copy of the puzzle
+                        new_puzzle[peg_ind].remove(disk)
+                        new_puzzle[index].append(disk)
+                        # in the new puzzle perform the move
+                        if len(target_peg) == 0 or target_peg[-1] > disk:
+                            children.append(new_puzzle)
+                            print(f"Safe move from {self.puzzle} to {new_puzzle} generated.") #generate safe state
+                        else:
+                            print(f"Unsafe move {self.puzzle} to {new_puzzle} marked as dead-end.") #mark unsafe states and do not explore them further
+        return children
--- search.py
+++ search.py
@@ -18,20 +18,20 @@ class Search:
 
     def bfs(self, root):
         open_list = []
-        visited = set()
+        visited = {}
 
         open_list.append(root)
         visited.add(self.puzzle_to_tuple(root.puzzle))
 
     def a_star_search(self, root):
         open_list = [] #similar to DFS implentation, initialize open list and visited set
-        visited = set()
+        visited = {}
 
         open_list.append(root) #append root to open list
         visited.add(self.puzzle_to_tuple(root.puzzle)) #add root to visited set
         #the puzzle_to_tuple function is called again to avoid hashable error
 
-        while True: 
+        while True:
             current_Node = open_list.pop(0) #pop element 0 from open list
             print(f"Searching node: {current_Node.puzzle}")
 
@@ -39,12 +39,12 @@ class Search:
                 path_to_solution = self.path_trace(current_Node) #return the path to the goal state
                 return path_to_solution
 
-            current_Node.generate_moves() #generate valid states/moves from current state
+            children = current_Node.generate_moves() #generate valid states/moves from current state
 
-            for current_child in current_Node.children: #for each child state
-                child_puzzle_tuple = self.puzzle_to_tuple(current_child.puzzle) #convert to tuple
-                if child_puzzle_tuple not in visited: #if child state not in visited
-                    open_list.append(current_child) #add child to open list
-                    visited.add(child_puzzle_tuple) #add child to visited set
+            for current_child in children:
+                child_puzzle_tuple = self.puzzle_to_tuple(current_child.state)
+                if child_puzzle_tuple not in visited:
+                    open_list.append(current_child)
+                    visited[child_puzzle_tuple] = self.puzzle_to_tuple(current_Node).add(child_puzzle_tuple)
  
             open_list.sort(key=lambda x: x.f) #anonymous function to sort open list by f value of states
