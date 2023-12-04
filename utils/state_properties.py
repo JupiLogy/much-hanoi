8191c2f17ab43f6e1ff6e95607e1b644a7e783fb
Jupy <jupiterian.is@gmail.com>
2023-12-04 19:24:02 +0000

    Creating utils

---
+++ utils/state_properties.py
@@ -0,0 +1,18 @@
+
+
+def num_pegs(state):
+    return(len(state))
+
+def num_discs(state):
+    return(max(max(state)))
+
+def validate_state(state, godmode=False):
+    if not godmode:
+        for lis in state:
+            if sorted(lis)!=lis:
+                return False
+    flatstate = [disc for peg in state for disc in peg]
+    if not (set(range(1,num_discs(state)+1)) == set(flatstate)\
+            and len(flatstate) == len(set(flatstate))):
+        return False
+    return True
